3f049103a839f760fd8daf32ed2b32913b9c43b7
SimRoad <montanez.simonemikaela@gmail.com>
2026-02-26 10:40:23 +0800

    Frontend Update

--- BMIcalculator/MainPage.xaml.cs
+++ BMIcalculator/MainPage.xaml.cs
@@ -16,18 +16,54 @@ public partial class MainPage : ContentPage
         {
             double toMeters = height / 100;
             double bmi = weight / (toMeters * toMeters);
+            string category = "";
 
-            string category = bmi switch
+            // string category = bmi switch
+            // {
+            //     < 0 => "Not possible",
+            //     < 18.5 => "Underweight",
+            //     < 25 => "Normal",
+            //     < 35 => "Obese",
+            //     _ => "Severely Obese"
+            // };
+
+            switch (bmi)
             {
-                < 0 => "Not possible",
-                < 18.5 => "Underweight",
-                < 25 => "Normal",
-                < 35 => "Obese",
-                _ => "Severely Obese"
-            };
+                case 0:
+                    category = "Not Possible";
+                    BodyType.BackgroundColor=Colors.Black;
+                    break;
+                case < 18.5:
+                    category = "Underweight";
+                    BodyType.BackgroundColor=Colors.Green;
+                    break;
+                case < 25:
+                    category = "Normal";
+                    BodyType.BackgroundColor=Colors.YellowGreen;
+                    break;
+                case < 30:
+                    category = "Overweight";
+                    BodyType.BackgroundColor = Colors.Yellow;
+                    break;
+                case < 35:
+                    category = "Obese";
+                    BodyType.BackgroundColor = Colors.Orange;
+                    break;
+                default:
+                    category = "Extremely Obese";
+                    BodyType.BackgroundColor = Colors.Red;
+                    break;
+            }
 
             ResultsLabel.Text = $"{bmi:F2}";
             BodyType.Text = $"{category}";
+            ResultsLayout.IsVisible = true;
         }
+    }
+
+    private void ToggleResults(object? sender, EventArgs e)
+    {
+        ResultsLayout.IsVisible = !ResultsLayout.IsVisible;
+        ImperialBtn.Text = (ImperialBtn.Text == "Imperial") ? "01" : "Imperial";
     }
 }
